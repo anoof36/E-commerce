859126d31e27ae75d1fe9310b02da31eacfdd492
Mohamed Anoof M <mohamedanoof586@gmail.com>
2024-10-01 21:26:20 +0530

    header changing

--- frontend/src/components/common/header/index.jsx
+++ frontend/src/components/common/header/index.jsx
@@ -1,13 +1,15 @@
 import React from "react";
+import style from "./index.module.css";
 
 const AppHeader = () => {
   return (
     <>
-    <header>
-      <nav>
-        
-      </nav>
-    </header>
+      <header>
+        <div className={`d-flex bg-success vw-100  ${style.container}`}>
+          <div className={`bg-primary w-50 pl-3 ${style.left}`}>VIBE</div> {/* Use backticks for template literal */}
+          <div className="bg-warning w-50">home</div>
+        </div>
+      </header>
     </>
   );
 };
