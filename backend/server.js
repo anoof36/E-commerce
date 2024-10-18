5d9d748b79e2678767c43d4efe332e7ae84abeb5
Mohamed Anoof M <mohamedanoof586@gmail.com>
2024-10-18 15:23:30 +0530

    upload folder added

--- backend/server.js
+++ backend/server.js
@@ -1,6 +1,17 @@
 import express from "express";
 import "dotenv/config";
 import dbConnect from "./config/mongoose.js";
+import multer from "multer"
+
+/* CEATING IMAGE STORAGE WITH MULTER LIBRARY */
+const storage = multer.dickStorag({
+  destination: (req, file, cb) => {
+    cb(null, 'uploads/'); //setting the folder where images will be saved
+  },
+  filename: (req, file, cb)=> {
+    cb(null, Date.now() + path.extname(file.originalname)) //  // Saving file with a unique name
+  }
+})
 
 /* C0NFIGURATOIN */
 const app = express();
